8027fcadcfc1fc6a56c5376f7989504a4f6c650b
Rui Oliveira <ruimail24@gmail.com>
2022-06-05 11:15:02 +0100

    Add the match table for the ST7701S

--- drivers/gpu/drm/panel/panel-sitronix-st7701s.c
+++ drivers/gpu/drm/panel/panel-sitronix-st7701s.c
@@ -3,3 +3,13 @@
  * Copyright (C) 2022, Rui Oliveira.
  * Author: Rui Oliveira <ruimail24@gmail.com>
  */
+
+#include <linux/module.h>
+#include <linux/mod_devicetable.h>
+
+static const struct of_device_id st7701s_of_match[] = {
+	{ .compatible = "sitronix,st7701s" },
+	{ .compatible = "jinglitai,jlt4013a" },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, y030xx067a_of_match);
